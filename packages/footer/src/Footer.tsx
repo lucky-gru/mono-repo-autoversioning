0c50bb4530677ef6b4750b1e41184d53ec093b38
hiroshi <hiroshi.tan57@gmail.com>
2023-05-26 16:04:51 +0000

    Update

--- packages/footer/src/Footer.tsx
+++ packages/footer/src/Footer.tsx
@@ -6,6 +6,7 @@ export function Footer() {
             Footer
             <div>updated footer</div>
             <div>updated footer 1</div>
+            <div>updated footer 1</div>
         </footer>
     );
 }
