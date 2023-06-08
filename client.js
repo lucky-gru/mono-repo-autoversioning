bce6e55d7e2d00dcdcf2868ecb98874bf27518a9
hiroshi <hiroshi.tan57@gmail.com>
2023-06-08 01:24:22 +0000

    Update

--- client.js
+++ client.js
@@ -1,71 +1,84 @@
 const axios = require("axios");
 
-const { Client } = require("pg");
+const keycloakUrl = "http://localhost:8080/auth";
 
 (async () => {
-  axios
-    .get("http://localhost:8080/auth")
-    .then(function (response) {
-      // handle success
-      console.log("8080", response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .finally(function () {
-      // always executed
-    });
+  try {
+    const response = await axios.get(`${keycloakUrl}`);
+    console.log("Keycloak response:", response.data);
+  } catch (error) {
+    console.error("Error fetching Keycloak data:", error);
+  }
 })();
 
-(async () => {
-  axios
-    .get("https://localhost:8080/auth")
-    .then(function (response) {
-      // handle success
-      console.log("8080s", response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .finally(function () {
-      // always executed
-    });
-})();
+// const axios = require("axios");
 
-const pgclient = new Client({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "example",
-  database: "keycloak_db",
-});
+// const { Client } = require("pg");
 
-pgclient.connect();
+// (async () => {
+//   axios
+//     .get("http://localhost:8080/auth")
+//     .then(function (response) {
+//       // handle success
+//       console.log("8080", response);
+//     })
+//     .catch(function (error) {
+//       // handle error
+//       console.log(error);
+//     })
+//     .finally(function () {
+//       // always executed
+//     });
+// })();
 
-const table =
-  "CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))";
-const text =
-  "INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *";
-const values = [
-  "Mona the",
-  "Octocat",
-  9,
-  "88 Colin P Kelly Jr St, San Francisco, CA 94107, United States",
-  "octocat@github.com",
-];
+// (async () => {
+//   axios
+//     .get("https://localhost:8080/auth")
+//     .then(function (response) {
+//       // handle success
+//       console.log("8080s", response);
+//     })
+//     .catch(function (error) {
+//       // handle error
+//       console.log(error);
+//     })
+//     .finally(function () {
+//       // always executed
+//     });
+// })();
 
-pgclient.query(table, (err, res) => {
-  if (err) throw err;
-});
+// const pgclient = new Client({
+//   host: "localhost",
+//   port: 5432,
+//   user: "postgres",
+//   password: "example",
+//   database: "keycloak_db",
+// });
 
-pgclient.query(text, values, (err, res) => {
-  if (err) throw err;
-});
+// pgclient.connect();
 
-pgclient.query("SELECT * FROM student", (err, res) => {
-  if (err) throw err;
-  console.log(err, res.rows); // Print the data in student table
-  pgclient.end();
-});
+// const table =
+//   "CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))";
+// const text =
+//   "INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *";
+// const values = [
+//   "Mona the",
+//   "Octocat",
+//   9,
+//   "88 Colin P Kelly Jr St, San Francisco, CA 94107, United States",
+//   "octocat@github.com",
+// ];
+
+// pgclient.query(table, (err, res) => {
+//   if (err) throw err;
+// });
+
+// pgclient.query(text, values, (err, res) => {
+//   if (err) throw err;
+// });
+
+// pgclient.query("SELECT * FROM student", (err, res) => {
+//   if (err) throw err;
+//   console.log(err, res.rows); // Print the data in student table
+//   pgclient.end();
+// });
